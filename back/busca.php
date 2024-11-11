add2be4d40e0b7619e26ac5c8f4718db512557dc
Diovani Stefanello Facco <93200912+diovani-f@users.noreply.github.com>
2024-11-11 16:33:00 -0300

    att2

--- back/busca.php
+++ back/busca.php
@@ -3,67 +3,67 @@
 include 'conexao.php';
 header('Content-Type: application/json');
 
-function buscarDisciplina() {
-    $tabela = 'disciplina';
 
+function buscarDisciplina() {
     // Isso vem tudo do js
     // -Obrigatorio-
     $data_inicio = '10-10-2020';
     $data_final = '17-10-2020';     //isso compreende uma semana
     // -Opcional-
-    $nome_disciplina = 'Matemática';
-    $id_disciplina = 1;
-    $professor = 'João';
+    $nome_disciplina = 0;
+    $id_disciplina = 0;
+    $professor = 0;
 
+    $sql = "select d.iddisciplina, d.nome, p.nome as professor from disciplina d join professor p ON p.idprofessor = d.professor_idprofessor";
     
-    $sql = "select d.iddisciplina, d.nome, p.nome as professor from disciplina d";
-    // quando o pedro colocar as colunas descomentar isso e tirar o where do sql
-    // $parametros = " where data_inicio = $data_inicio and data_final = $data_final";
+    
+    // correto
+    // $sql = "select d.iddisciplina, d.nome, p.nome as professor from disciplina d join professor p ON p.idprofessor = d.professor_idprofessor 
+    // where d.data_inicio < $data_inicio and d.data_fim > $data_fim ";
+
+
+    //data de inicio e fim pesquisada, tem que estar dentro a data_inicio e fim do banco
+    // dias da semana
+
 
     $parametros = " where ";
-    
+
     if($professor){
-        $sql .= " join professor p ON p.idprofessor = d.professor_idprofessor";
-        //quando aplicar as outras mudanças colocar and aqui tbm
-        $parametros .= " p.nome like '" . $professor . "%'";
+        $parametros .= " and p.nome like '" . utf8_decode($professor) . "%'";
     }
 
     if($id_disciplina){
-
         $parametros .= " and d.iddisciplina = " . $id_disciplina;
     }
 
     if($nome_disciplina){
-        $parametros .= " and d.nome like '" . $nome_disciplina . "%'";
+        $parametros .= " and d.nome like '" . utf8_decode($nome_disciplina) . "%'";
     }
 
-    
-    $sql .= $parametros;
-    
-    file_put_contents('teste.txt',print_r($sql, true) . PHP_EOL ,FILE_APPEND);
-
+    // $sql .= $parametros;    
 
     $conn = conectarBanco();
-
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $resultado = $stmt->get_result();
 
+    // Inicializa um array para armazenar os dados
+    $dados = [];
+
     if ($resultado->num_rows > 0) {
         while ($row = $resultado->fetch_assoc()) {
+            foreach ($row as $key => $value) {
+                $row[$key] = utf8_encode($value);
+            }
             $dados[] = $row;
         }
     }
 
-    file_put_contents('$dados.txt',print_r($dados, true) . PHP_EOL ,FILE_APPEND);
-
-    // Fechar a conexão e a consulta
     $stmt->close();
     $conn->close();
+
+    echo json_encode($dados);
 }
+
 buscarDisciplina();
-
-//  Inicialmente: Pela Data que vai estar selecionada no calendario. (Talvez pelo curso)
-
-// Campos de busca opcional: Professor, Id da disciplina, Id do curso 
 ?>
--- back/conexao.php
+++ back/conexao.php
@@ -1,20 +1,18 @@
 <?php
 // conexao.php
 
-// Configurações de conexão
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "mydb";
 
-// Função para conectar ao banco de dados
 function conectarBanco() {
     global $servername, $username, $password, $dbname;
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Verifica se a conexão foi bem-sucedida
+
     if ($conn->connect_error) {
-        die("Falha na conexão: " . $conn->connect_error);
+        die("Falha na conexao: " . $conn->connect_error);
     }
     return $conn;
 }
