466c586ceb9fa016365a248353d1c7ba74f3b305
Diovani Stefanello Facco <93200912+diovani-f@users.noreply.github.com>
2024-11-11 15:14:14 -0300

    Att

--- back/busca.php
+++ back/busca.php
@@ -6,30 +6,46 @@ header('Content-Type: application/json');
 function buscarDisciplina() {
     $tabela = 'disciplina';
 
+    // Isso vem tudo do js
     // -Obrigatorio-
     $data_inicio = '10-10-2020';
     $data_final = '17-10-2020';     //isso compreende uma semana
     // -Opcional-
-    $nome_disciplina = 'Matematica';
-    $id_disciplina = 12;
-    $professor = 'Sergio';
+    $nome_disciplina = 'Matemática';
+    $id_disciplina = 1;
+    $professor = 'João';
 
     
-    $sql = "select * from disciplina";
-    
-    $parametros = " where data_inicio = $data_inicio and data_final = $data_final";
+    $sql = "select d.iddisciplina, d.nome, p.nome as professor from disciplina d";
+    // quando o pedro colocar as colunas descomentar isso e tirar o where do sql
+    // $parametros = " where data_inicio = $data_inicio and data_final = $data_final";
+
+    $parametros = " where ";
     
     if($professor){
-        $parametros .= " and f.professor = " . $professor . "%";
+        $sql .= " join professor p ON p.idprofessor = d.professor_idprofessor";
+        //quando aplicar as outras mudanças colocar and aqui tbm
+        $parametros .= " p.nome like '" . $professor . "%'";
     }
 
-    file_put_contents('teste.txt',print_r($parametros, true) . PHP_EOL ,FILE_APPEND);
+    if($id_disciplina){
+
+        $parametros .= " and d.iddisciplina = " . $id_disciplina;
+    }
+
+    if($nome_disciplina){
+        $parametros .= " and d.nome like '" . $nome_disciplina . "%'";
+    }
+
     
+    $sql .= $parametros;
+    
+    file_put_contents('teste.txt',print_r($sql, true) . PHP_EOL ,FILE_APPEND);
 
 
     $conn = conectarBanco();
 
-    $stmt = $conn->prepare("SELECT * FROM $tabela");
+    $stmt = $conn->prepare($sql);
     $stmt->execute();
     $resultado = $stmt->get_result();
 
@@ -39,16 +55,15 @@ function buscarDisciplina() {
         }
     }
 
+    file_put_contents('$dados.txt',print_r($dados, true) . PHP_EOL ,FILE_APPEND);
+
     // Fechar a conexão e a consulta
     $stmt->close();
     $conn->close();
 }
 buscarDisciplina();
+
+//  Inicialmente: Pela Data que vai estar selecionada no calendario. (Talvez pelo curso)
+
+// Campos de busca opcional: Professor, Id da disciplina, Id do curso 
 ?>
-
-
-
-
-<!-- Inicialmente: Pela Data que vai estar selecionada no calendario. (Talvez pelo curso)
-
-Campos de busca opcional: Professor, Id da disciplina, Id do curso -->
