013f7b168720f1cad9a54b2f1183e9c925a59728
Diovani Stefanello Facco <93200912+diovani-f@users.noreply.github.com>
2024-11-15 15:08:08 -0300

    Update triggers.sql

--- ScriptSQL/triggers.sql
+++ ScriptSQL/triggers.sql
@@ -12,3 +12,25 @@ END;
 //
 
 DELIMITER ;
+
+
+
+#salvar historico de exclusão
+
+DELIMITER $$
+
+CREATE TRIGGER trg_disciplina_delete
+BEFORE DELETE ON disciplina
+FOR EACH ROW
+BEGIN
+    -- Insere a ação no histórico
+    INSERT INTO historico_disciplinas (id_coordenador, id_disciplina, acao, data_hora)
+    VALUES (
+        (SELECT id_coordenador FROM curso WHERE id = OLD.id_curso), -- Coordenador associado ao curso
+        OLD.id, -- ID da disciplina que será excluída
+        'remoção', -- Ação de remoção
+        NOW() -- Data e hora atuais
+    );
+END$$
+
+DELIMITER ;
