3811205d94757bd5bb77b2b0a5d75bd3f5847393
Diovani Stefanello Facco <93200912+diovani-f@users.noreply.github.com>
2024-11-27 16:53:41 -0300

    Update Banco novo.sql

--- ScriptSQL/Banco novo.sql
+++ ScriptSQL/Banco novo.sql
@@ -25,6 +25,7 @@ CREATE TABLE sala (
 );
 
 -- Criação da tabela disciplina
+
 CREATE TABLE disciplina (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
@@ -33,9 +34,9 @@ CREATE TABLE disciplina (
     vagas_disponiveis INT NOT NULL,
     id_professor INT,
     id_curso INT,
-    FOREIGN KEY (id_professor) REFERENCES professor(id),
-    FOREIGN KEY (id_curso) REFERENCES curso(id),
-    FOREIGN KEY (id_sala) REFERENCES sala(numero)
+    FOREIGN KEY (id_professor) REFERENCES professor(id) ON DELETE SET NULL,
+    FOREIGN KEY (id_curso) REFERENCES curso(id) ON DELETE SET NULL,
+    FOREIGN KEY (id_sala) REFERENCES sala(numero) ON DELETE SET NULL
 );
 
 -- Tabela que guarda o histórico de inserções, remoções e alterações nas disciplinas
@@ -44,9 +45,7 @@ CREATE TABLE historico_disciplinas (
     id_coordenador INT NOT NULL,
     id_disciplina INT NOT NULL,
     acao ENUM('inserção', 'remoção', 'edição') NOT NULL,
-    data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_coordenador) REFERENCES professor(id),
-    FOREIGN KEY (id_disciplina) REFERENCES disciplina(id)
+    data_hora DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Tabela que guarda os horários e dias que cada aula de uma determinada disciplina é ministrada
@@ -72,3 +71,12 @@ CREATE TABLE usuario (
 
 -- Relacionamento entre coordenador e curso
 ALTER TABLE curso ADD FOREIGN KEY (id_coordenador) REFERENCES usuario(id);
+
+-- Adiciona a coluna id_curso em historico_disciplinas
+ALTER TABLE historico_disciplinas ADD COLUMN id_curso INT;
+
+-- Cria a foreign key entre historico_disciplinas e curso
+ALTER TABLE historico_disciplinas
+ADD CONSTRAINT fk_historico_curso
+FOREIGN KEY (id_curso) REFERENCES curso(id)
+ON DELETE CASCADE;
